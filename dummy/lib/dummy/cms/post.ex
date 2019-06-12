74676f59366a86fd06d1d3df7e831e9c3ca4198c
zven21 <andzven@gmail.com>
2019-06-12 14:16:55 +0800

    it's can running.

--- dummy/lib/dummy/cms/post.ex
+++ dummy/lib/dummy/cms/post.ex
@@ -3,30 +3,29 @@ defmodule Dummy.CMS.Post do
   import Ecto.Changeset
 
   schema "posts" do
+    field :title, :string
     field :body, :string
     field :last_comment_id, :integer
     field :last_commented_at, :naive_datetime
-    field :title, :string
     field :user_id, :integer
 
     timestamps()
   end
 
-  @doc false
   def changeset(post, attrs) do
-    optional_attrs = ~w(
+    optional_fields = ~w(
       last_commented_at
       last_comment_id
       user_id
     )a
 
-    required_attrs = ~w(
+    required_fields = ~w(
       title
       body
     )a
 
     post
-    |> cast(attrs, optional_attrs ++ required_attrs)
-    |> validate_required(required_attrs)
+    |> cast(attrs, optional_fields ++ required_fields)
+    |> validate_required(required_fields)
   end
 end
---
+++ dummy/priv/repo/migrations/20190611100813_add_versions_table.exs
@@ -0,0 +1,18 @@
+defmodule Dummy.Repo.Migrations.AddVersionsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:versions) do
+      add :item_type, :string, null: false
+      add :item_id, :integer, null: false
+      add :item_prev, :map, null: false
+      add :item_current, :map, null: false
+      add :origin, :string
+      add :meta, :map
+
+      add :inserted_at, :utc_datetime, null: false
+    end
+
+    create index(:versions, [:item_type, :item_id])
+  end
+end
